726b20cdf1a33faca411a5a2ce760989318078c5
inchanS <song@inchan.dev>
2025-06-04 02:56:19 +0900

    style: fix formatting for toolbar button template  - 코드를 시각적으로 정리하여 가독성을 개선함  - 템플릿의 공백 및 들여쓰기 수정 완료

--- index.js
+++ index.js
@@ -24,15 +24,15 @@ const main = () => {
     logseq.App.registerUIItem('toolbar', {
         key: 'block-extractor',
         template: `
-  <a class="button" data-on-click="showExtractDialog" title="Extract Blocks" 
-     style="padding: 4px 6px;">
-    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-      <polyline points="7,10 12,15 17,10"></polyline>
-      <line x1="12" y1="15" x2="12" y2="3"></line>
-    </svg>
-  </a>
-`
+          <a class="button" data-on-click="showExtractDialog" title="Extract Blocks" 
+             style="padding: 4px 6px;">
+            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+              <polyline points="7,10 12,15 17,10"></polyline>
+              <line x1="12" y1="15" x2="12" y2="3"></line>
+            </svg>
+          </a>
+        `
     });
 
     // 모든 페이지 목록을 가져오는 함수
