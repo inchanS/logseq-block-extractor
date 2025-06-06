b3c87ab78377f40784553c0ea896fd10287b00d0
charlie <xyhp915@qq.com>
2025-06-06 12:26:08 +0800

    chore: set index.html as entry

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,7 @@ export default defineConfig({
     build: {
         target: 'esnext',
         minify: false, // Disable minification for debugging
-        outDir: 'dist',
-        rollupOptions: {
-            input: 'src/index.ts',
-            output: {
-                entryFileNames: 'index.js',
-                format: 'es'
-            }
-        }
+        outDir: 'dist'
     },
     base: './'
 })
